82552068867834410f39ac68586f42a39b35ac2f
alefranco41 <alessandro.franceschini.2002@gmail.com>
2024-02-04 20:59:37 +0100

    base per il progetto

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Progetto_IR
+
+Usage:
+1) Install dependencies on requirements.txt
+2) Unpack CSVdata.rar inside /src directory
+3) python3 whooshIndex.py
+4) wait about 2 hours :D
+5) python3 main.py
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from whooshIndex import getIndex, fieldList
+from src.whooshIndex import getIndex, fieldList
 from whoosh.qparser import MultifieldParser #enable multiple field search
 from whoosh import scoring #set up the scoring system
 import shutil #for printing purposes
